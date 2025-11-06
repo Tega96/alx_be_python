7b5c70904bb84d466276cb2de6ba0dfbc7282329
Otega <otiteotega@gmail.com>
2025-11-06 22:55:15 +0100

    basic arithemetic exercise

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,15 @@
+"""
+Write a Python script that performs basic arithmetic operations with 
+two predefined numbers.
+"""
+
+number1 = 10
+number2 = 5
+
+sum = number1 + number2
+difference = number2 - number1
+product = number1 * number2
+
+print("The sum of ", number1, "and", number2, "is", sum)
+print("The difference between ", number1, "and", number2, "is", difference)
+print("The product of ", number1, "and", number2, "is", product)
