64c303432f594d3cc309ec1d6d93c546b5d2fc36
Otega Otite <otiteotega@gmail.com>
2025-11-06 23:39:45 +0100

    hours to seconds

---
+++ python_introduction/hours_to_seconds.py
@@ -0,0 +1,9 @@
+"""
+Script that converts a specified number of hours into seconds. 
+"""
+
+hours = 2
+seconds = 60 * 60
+result = hours * seconds
+
+print(hours, "hours(s) is", result, 'seconds.')
