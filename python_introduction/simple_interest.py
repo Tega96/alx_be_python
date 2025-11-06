9b73c1e34f646b2f95e2d36c28ff31bd3fce1d83
Otega Otite <otegaotite@gmail.com>
2025-11-06 23:10:02 +0100

    simple interest

---
+++ python_introduction/simple_interest.py
@@ -0,0 +1,12 @@
+"""
+Python Script that calculates the simple interest earned on an investment over a period of time. 
+Formular: (I = P * R * T)
+"""
+
+principal = 1000
+rate = 0.05
+time = 3
+
+interest = principal * rate * time
+print("The simple interest is: ", interest)
+print 
