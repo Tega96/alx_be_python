0c5b1714b91ec1e30cc3ede3ddd3aaedebd54d2a
Otega Otite <otiteotega@gmail.com>
2025-11-17 23:42:36 +0100

    Arithmetic operation function

---
+++ fns_and_dsa/arithmetic_operations.py
@@ -0,0 +1,10 @@
+
+def perform_operation(num1: float, num2: float, operation: str):
+    if operation == 'add':
+        num1 + num2
+    elif operation == 'subtract':
+        num1 - num2
+    elif operation == 'divide':
+        num1 - num2
+    elif operation == 'multiply':
+        num1 * num2
