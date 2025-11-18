84b142521b0567ac5974acbb855194c4acfbacf6
Otega Otite <otiteotega@gmail.com>
2025-11-18 05:58:59 +0100

    corrections

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -1,14 +1,14 @@
 
 def perform_operation(num1, num2, operation):
     if operation == 'add':
-        num1 + num2
+        return num1 + num2
     elif operation == 'subtract':
-        num1 - num2
+        return num1 - num2
     elif operation == 'multiply':
-        num1 * num2
+        return num1 * num2
     elif operation == 'divide':
         if num1 == 0 or num2 == 0:
-            print('input a number greater than zero')
+            return 'input a number greater than zero'
         else:
-            num1 / num2
+            return num1 / num2
 
