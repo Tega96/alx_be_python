1c4557fe1a12fb24459263987a2508962376e69d
Otega Otite <otiteotega@gmail.com>
2025-12-24 10:24:41 +0100

    control flow

--- control-flow/weather_advice.py
+++ control-flow/weather_advice.py
@@ -1,10 +1,10 @@
 curr_weather = input("What's the weather like today? (sunny/rainy/cold):")
 
 if curr_weather == "sunny":
-    print('Wear a t-shirt and sunglasses')
+    print("Wear a t-shirt and sunglasses.")
 elif curr_weather == "rainy":
-    print('Don\'t forget your umbrella and raincoat')
+    print("Don't forget your umbrella and a raincoat.")
 elif curr_weather == "cold":
     print("Make sure to wear a warm coat and a scarf.")
 else:
-    print("Sorry, I don\'t have recommendations for this weather. ")
+    print("Sorry, I don't have recommendations for this weather.")
